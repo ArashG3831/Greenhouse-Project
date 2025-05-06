f185ec910c1f661b489cb8a7c9fdb847eecc0c5c
Arash_g <arash.ganjei92@gmail.com>
2025-05-06 20:38:49 +0330

    MySQL fix urls

--- sensor/urls.py
+++ sensor/urls.py
@@ -12,9 +12,9 @@ from .views import (
 urlpatterns = [
     path('api/get_data/', get_data, name='get_data'),
     path('api/receive_data/', receive_data, name='receive_data'),
-    path('api/get_predictions/', get_predictions, name='get_predictions'),
+    # path('api/get_predictions/', get_predictions, name='get_predictions'),
     path('api/get_control_state/', get_control_state, name='get_control_state'),
     path('api/set_control_state/', set_control_state, name='set_control_state'),
-    path('api/smartthings_webhook/', smartthings_webhook, name='smartthings_webhook'),  # ✅ Added this new
-    path('api/update-fan-status/', update_fan_status, name='update_fan_status'),
+    # path('api/smartthings_webhook/', smartthings_webhook, name='smartthings_webhook'),  # ✅ Added this new
+    # path('api/update-fan-status/', update_fan_status, name='update_fan_status'),
 ]
